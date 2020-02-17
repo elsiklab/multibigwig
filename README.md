66a68a53766b90822d443fa50d282678f77f1c13
Colin <colin.diesh@gmail.com>
2020-02-17 14:39:17 -0500

    Add lineWidth

--- README.md
+++ README.md
@@ -95,7 +95,15 @@ Same thing in trackList.json
 
 ### Main configuration
 
-* urlTemplates - An array of subtracks, containing the url for a BW file, name, and optionally color for subtrack labels. Each one can have the following properties: name, description, color, nonCont, and url.
+* urlTemplates - An array of subtracks, containing the url for a BW file, name, and optionally color for subtrack labels. Each one can have the following properties:
+
+  * name (name or label for the subtrack)
+  * description (extended description)
+  * color (a color for the line)
+  * nonCont (discontinuous drawing of the line, or "pointillism" style)
+  * url (a URL to a bigwig file)
+  * lineWidth (a number)
+  * storeClass (alternative to URL that allows full storeClass initialization, see below)
 
 Example
 
@@ -106,7 +114,8 @@ Example
 The configuration can also specify a storeClass if something other than BigWig is used
 
     urlTemplates+=json:{"storeClass": "JBrowse/Store/SeqFeature/BedGraphTabix", "url":"volvox_microarray.bg.gz", "name": "volvox_positive", "color": "#235"}
-    urlTemplates+=json:{"storeClass": "JBrowse/Store/SeqFeature/BedGraphTabix", "url":"volvox_microarray_negative.bg.gz", "name": "volvox_negative", "color": "#a54"}
+    urlTemplates+=json:{"storeClass": "JBrowse/Store/SeqFeature/BedGraphTabix", "url":"volvox_microarray_negative.bg.gz", "name": "volvox_negative", "color": "#a54", "lineWith": 3}
+
 
 
 
--- js/View/Track/MultiWiggle/MultiXYPlot.js
+++ js/View/Track/MultiWiggle/MultiXYPlot.js
@@ -118,19 +118,20 @@ function (
                     var score = toY(s.score, source);
                     var elt = this.labels.find(function (l) { return l.name === f.get('source'); });
                     var color = elt.color;
+                    var height = elt.lineWidth || 1;
                     var nonCont = elt.nonCont;
                     if (score <= canvasHeight || score > originY) { // if the rectangle is visible at all
                         if (score <= originY) {
                             // bar goes upward
                             if (nonCont) {
                                 context.fillStyle = color;
-                                var height = 1;
                                 if (elt.fill) {
                                     height = originY - score + 1;
                                 }
                                 thisB._fillRectMod(context, i, score, 1, height);
                             } else {
                                 context.strokeStyle = color;
+                                context.lineWidth = height;
                                 context.beginPath();
                                 context.moveTo(i, initMap[source]);
                                 context.lineTo(i + 1, score);
@@ -149,6 +150,7 @@ function (
                             thisB._fillRectMod(context, i, top, 1,  heightm);
                         } else {
                             context.strokeStyle = color;
+                            context.lineWidth = height;
                             context.beginPath();
                             context.moveTo(i, initMap[source]);
                             context.lineTo(i + 1, score);
